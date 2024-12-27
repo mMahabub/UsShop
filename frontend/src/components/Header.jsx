300d1f4f3881189919eef6af0e27b489527b7a89
Mahabub Alam <mahabubalam2k10@gmail.com>
2024-12-26 21:12:19 -0500

    Create utilies and update header file also cart the product in localstorage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Header() {
+
+  const { cartItems}  = useSelector((state) => state.cart)
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -17,6 +20,15 @@ function Header() {
             <Nav className="ms-auto">
               <Nav.Link as={Link} to="/cart">
                 <FaShoppingCart /> Cart
+                {
+                 cartItems.length > 0 && (
+                  <Badge pill bg='success' style={{ marginLeft: '5px'}}>
+                    {cartItems.reduce((a, c) =>  a + c.qty, 0)}
+
+                  </Badge>
+                 )
+                
+                }
               </Nav.Link>
               <Nav.Link as={Link} to="/login">
                 <FaUser /> Sign In
