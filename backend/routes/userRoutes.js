58ea02ac15d6411170fcd9cf7fde63ab54031156
Mahabub Alam <mahabubalam2k10@gmail.com>
2024-12-27 11:08:44 -0500

    Create jwt and example.env and set up auth user

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from '../controllers/userController.js';
+// import { protect, admin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').post(registerUser).get( getUsers);
+// router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router
+  .route('/profile')
+  .get( getUserProfile)
+  .put( updateUserProfile);
+router
+  .route('/:id')
+  .delete( deleteUser)
+  .get( getUserById)
+  .put( updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js'
 const port = process.env.PORT || 5001;
 connectDB();
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Api is running......');
@@ -16,6 +20,7 @@ app.get('/', (req, res) => {
 
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
